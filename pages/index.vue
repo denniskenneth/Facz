88a8c594e4c5e2e163990a4d0bcdfc40792fd57c
Dennis Kenneth <dennyify123@gmail.com>
2021-06-18 17:39:40 +0000

    first commit

--- pages/index.vue
+++ pages/index.vue
@@ -58,7 +58,7 @@ export default {
   font-size: 3.5rem;
 }
 
-/* BTN STYLING */
+
 /* .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
